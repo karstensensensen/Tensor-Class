b9570e3bf426828c005807948606a8ca573125ab
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2020-10-21 09:24:35 +0200

    moved enum file

---
+++ TensorMac/TensorMac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TensorMac
+//
+//  Created by Simon Levin on 21/10/2020.
+//  Copyright © 2020 Simon Levin. All rights reserved.
+//
+
+#include <iostream>
+#include "Tensor.h"
+
+int main(int argc, const char * argv[]) {
+	TSlib::Tensor<int>({});
+	return 0;
+}
