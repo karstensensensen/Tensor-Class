bfde2bc1d5d6daa860141b68fa70957963d8f81f
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2020-10-21 10:22:26 +0200

    commit

--- TensorMac/TensorMac/main.cpp
+++ TensorMac/TensorMac/main.cpp
@@ -1,17 +1,11 @@
-//
-//  main.cpp
-//  TensorMac
-//
-//  Created by Simon Levin on 21/10/2020.
-//  Copyright © 2020 Simon Levin. All rights reserved.
-//
-
 #include <iostream>
 #include "Tensor.h"
 
 int main(int argc, const char * argv[]) {
 	std::cout << "Tensor:\n";
 	TSlib::Tensor<int> tensor({5, 5, 5}, 25);
-	std::cout << tensor + 5;
+	TSlib::Tensor<int> tensor2({3, 3, 3}, 25);
+	tensor2.Reshape({5, 5, 5});
+	std::cout << tensor + tensor2;
 	return 0;
 }
