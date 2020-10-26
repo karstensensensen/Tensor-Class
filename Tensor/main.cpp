bf5a1d93864562310831bb868f09a3d52860658b
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2020-10-26 15:40:38 +0100

    commit

--- Tensor/main.cpp
+++ Tensor/main.cpp
@@ -1,61 +1,67 @@
-#define PROFILING 2
-#include <iostream>
-#include "Tensor.h"
-#include <Profiler.h>
-#include <fstream>
-#include <intrin.h>
-
-using namespace Profiling;
-using namespace TSlib;
-
-__kernel__ kernel(CUDATensor3D<uint32_t> tensor, int val)
-{
-	if (tensor.in_bounds())
-	{
-		for (int i = 0; i < 10000; i++)
-		{
-			tensor.At() += val;
-		}
-		
-		if (tensor.offset_bounds(2))
-		{
-			for (int i = 0; i < 10000; i++)
-			{
-			
-					tensor.Offset(2) += val;
-			}
-		}
-	}
-}
-
-size_t generator(const size_t& i)
-{
-	return i % 10;
-}
-
-size_t generator2(const size_t& i)
-{
-	return i % 10;
-}
-
-__kernel__ k3D(CUDATensor3D<uint32_t> tensor, CUDATensor3D<uint32_t> other)
-{}
-
-int main()
-{
-	Profiler::Get().BeginSession("CUDA_SESSION", "CUDA_PROFILE.json");
-	Profiler::Get().margin = 50;
-	
-	CUDAInitialize();
-
-	try
-	{
-
-	}
-	catch (std::exception& e)
-	{
-		std::cout << e.what();
-	}
-
-	Profiler::Get().EndSession();
-}
+//#define PROFILING 2
+//#include <iostream>
+//#include "Tensor.h"
+//#include <Profiler.h>
+//#include <fstream>
+//#include <intrin.h>
+//
+//using namespace Profiling;
+//using namespace TSlib;
+//
+//__kernel__ kernel(CUDATensor3D<uint32_t> tensor, int val)
+//{
+//	if (tensor.in_bounds())
+//	{
+//		for (int i = 0; i < 10000; i++)
+//		{
+//			tensor.At() += val;
+//		}
+//		
+//		if (tensor.offset_bounds(2))
+//		{
+//			for (int i = 0; i < 10000; i++)
+//			{
+//			
+//					tensor.Offset(2) += val;
+//			}
+//		}
+//	}
+//}
+//
+//size_t generator(const size_t& i)
+//{
+//	return i % 10;
+//}
+//
+//size_t generator2(const size_t& i)
+//{
+//	return i % 10;
+//}
+//
+//__kernel__ k3D(CUDATensor3D<char> tensor)
+//{
+//	tensor.At() += 2;
+//	tensor.At() += rand()%100;
+//	tensor.At() *= 2;
+//	tensor.At() += tensor.Offset(1);
+//}
+//
+//int main()
+//{
+//	Profiler::Get().BeginSession("CUDA_SESSION", "CUDA_PROFILE.json");
+//	Profiler::Get().margin = 50;
+//	
+//	CUDAInitialize();
+//
+//	try
+//	{
+//		Tensor<char> tensor({ 1024, 1024, 1024, 8 });
+//		tensor.Kernel3D(Layout3D(), k3D);
+//	}
+//	catch (std::exception& e)
+//	{
+//		std::cout << e.what();
+//	}
+//
+//	Profiler::Get().EndSession();
+//}
