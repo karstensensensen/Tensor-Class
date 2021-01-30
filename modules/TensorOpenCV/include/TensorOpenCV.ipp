27ba457438b804226d0b1b7eaf47f70bbc14e4b7
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-01-31 00:11:25 +0100

    fixed functions in source file not being template specializations

--- modules/TensorOpenCV/include/TensorOpenCV.ipp
+++ modules/TensorOpenCV/include/TensorOpenCV.ipp
@@ -2,7 +2,8 @@
 
 namespace TSlib {
 	namespace Tools {
-		template<typename T, Mode device = default_device>
+
+		template<typename T, Mode device>
 		Tensor<T, device> MatToTensor(const cv::Mat& source)
 		{
 			Tensor<T, device> return_tensor({ source.rows, source.cols, source.channels() });
--- modules/TensorOpenCV/src/TensorOpenCV.cpp
+++ modules/TensorOpenCV/src/TensorOpenCV.cpp
@@ -2,8 +2,27 @@
 #include "TensorOpenCV.h"
 #include <random>
 
-template<TSlib::Mode device>
-cv::Mat TSlib::Tools::TensorToMat(const Tensor<unsigned char, device>& source)
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<unsigned char, Mode::CPU>& source)
+{
+	if (source.Dims() != 3)
+	{
+		throw TSlib::BadShape("Shape must have excactly 3 dimensions (width, height, channels)", source.Shape());
+	}
+	else if (source.Shape()[2] > 4)
+	{
+		throw TSlib::BadShape("There cannot be more than 4 channels (A, R, G, B)", source.Shape());
+	}
+
+	const std::vector<size_t>& shape = source.Shape();
+
+	cv::Mat return_mat(cv::Size(int(shape[0]), int(shape[1])), CV_8UC(int(shape[2])));
+	memcpy(return_mat.data, source.Data(), source.size());
+
+	return return_mat;
+}
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<unsigned char, Mode::GPU>& source)
 {
 	if (source.Dims() != 3)
 	{
@@ -22,8 +41,27 @@ cv::Mat TSlib::Tools::TensorToMat(const Tensor<unsigned char, device>& source)
 	return return_mat;
 }
 
-template<TSlib::Mode device>
-cv::Mat TSlib::Tools::TensorToMat(const Tensor<signed char, device>& source)
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<signed char, Mode::CPU>& source)
+{
+	if (source.Dims() != 3)
+	{
+		throw TSlib::BadShape("Shape must have excactly 3 dimensions (width, height, channels)", source.Shape());
+	}
+	else if (source.Shape()[2] > 4)
+	{
+		throw TSlib::BadShape("There cannot be more than 4 channels (A, R, G, B)", source.Shape());
+	}
+
+	const std::vector<size_t>& shape = source.Shape();
+
+	cv::Mat return_mat(cv::Size(int(shape[0]), int(shape[1])), CV_8SC(int(shape[2])));
+	memcpy(return_mat.data, source.Data(), source.size());
+
+	return return_mat;
+}
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<signed char, Mode::GPU>& source)
 {
 	if (source.Dims() != 3)
 	{
@@ -42,8 +80,27 @@ cv::Mat TSlib::Tools::TensorToMat(const Tensor<signed char, device>& source)
 	return return_mat;
 }
 
-template<TSlib::Mode device>
-cv::Mat TSlib::Tools::TensorToMat(const Tensor<unsigned short, device>& source)
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<unsigned short, Mode::CPU>& source)
+{
+	if (source.Dims() != 3)
+	{
+		throw TSlib::BadShape("Shape must have excactly 3 dimensions (width, height, channels)", source.Shape());
+	}
+	else if (source.Shape()[2] > 4)
+	{
+		throw TSlib::BadShape("There cannot be more than 4 channels (A, R, G, B)", source.Shape());
+	}
+
+	const std::vector<size_t>& shape = source.Shape();
+
+	cv::Mat return_mat(cv::Size(int(shape[0]), int(shape[1])), CV_16UC(int(shape[2])));
+	memcpy(return_mat.data, source.Data(), source.size());
+
+	return return_mat;
+}
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<unsigned short, Mode::GPU>& source)
 {
 	if (source.Dims() != 3)
 	{
@@ -62,8 +119,27 @@ cv::Mat TSlib::Tools::TensorToMat(const Tensor<unsigned short, device>& source)
 	return return_mat;
 }
 
-template<TSlib::Mode device>
-cv::Mat TSlib::Tools::TensorToMat(const Tensor<short, device>& source)
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<short, Mode::CPU>& source)
+{
+	if (source.Dims() != 3)
+	{
+		throw TSlib::BadShape("Shape must have excactly 3 dimensions (width, height, channels)", source.Shape());
+	}
+	else if (source.Shape()[2] > 4)
+	{
+		throw TSlib::BadShape("There cannot be more than 4 channels (A, R, G, B)", source.Shape());
+	}
+
+	const std::vector<size_t>& shape = source.Shape();
+
+	cv::Mat return_mat(cv::Size(int(shape[0]), int(shape[1])), CV_16SC(int(shape[2])));
+	memcpy(return_mat.data, source.Data(), source.size());
+
+	return return_mat;
+}
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<short, Mode::GPU>& source)
 {
 	if (source.Dims() != 3)
 	{
@@ -82,8 +158,27 @@ cv::Mat TSlib::Tools::TensorToMat(const Tensor<short, device>& source)
 	return return_mat;
 }
 
-template<TSlib::Mode device>
-cv::Mat TSlib::Tools::TensorToMat(const Tensor<int, device>& source)
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<int, Mode::CPU>& source)
+{
+	if (source.Dims() != 3)
+	{
+		throw TSlib::BadShape("Shape must have excactly 3 dimensions (width, height, channels)", source.Shape());
+	}
+	else if (source.Shape()[2] > 4)
+	{
+		throw TSlib::BadShape("There cannot be more than 4 channels (A, R, G, B)", source.Shape());
+	}
+
+	const std::vector<size_t>& shape = source.Shape();
+
+	cv::Mat return_mat(cv::Size(int(shape[0]), int(shape[1])), CV_32SC(int(shape[2])));
+	memcpy(return_mat.data, source.Data(), source.size());
+
+	return return_mat;
+}
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<int, Mode::GPU>& source)
 {
 	if (source.Dims() != 3)
 	{
@@ -102,8 +197,27 @@ cv::Mat TSlib::Tools::TensorToMat(const Tensor<int, device>& source)
 	return return_mat;
 }
 
-template<TSlib::Mode device>
-cv::Mat TSlib::Tools::TensorToMat(const Tensor<float, device>& source)
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<float, Mode::CPU>& source)
+{
+	if (source.Dims() != 3)
+	{
+		throw TSlib::BadShape("Shape must have excactly 3 dimensions (width, height, channels)", source.Shape());
+	}
+	else if (source.Shape()[2] > 4)
+	{
+		throw TSlib::BadShape("There cannot be more than 4 channels (A, R, G, B)", source.Shape());
+	}
+
+	const std::vector<size_t>& shape = source.Shape();
+
+	cv::Mat return_mat(cv::Size(int(shape[0]), int(shape[1])), CV_32FC(int(shape[2])));
+	memcpy(return_mat.data, source.Data(), source.size());
+
+	return return_mat;
+}
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<float, Mode::GPU>& source)
 {
 	if (source.Dims() != 3)
 	{
@@ -122,8 +236,27 @@ cv::Mat TSlib::Tools::TensorToMat(const Tensor<float, device>& source)
 	return return_mat;
 }
 
-template<TSlib::Mode device>
-cv::Mat TSlib::Tools::TensorToMat(const Tensor<double, device>& source)
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<double, Mode::CPU>& source)
+{
+	if (source.Dims() != 3)
+	{
+		throw TSlib::BadShape("Shape must have excactly 3 dimensions (width, height, channels)", source.Shape());
+	}
+	else if (source.Shape()[2] > 4)
+	{
+		throw TSlib::BadShape("There cannot be more than 4 channels (A, R, G, B)", source.Shape());
+	}
+
+	const std::vector<size_t>& shape = source.Shape();
+
+	cv::Mat return_mat(cv::Size(int(shape[0]), int(shape[1])), CV_64FC(int(shape[2])));
+	memcpy(return_mat.data, source.Data(), source.size());
+
+	return return_mat;
+}
+template<>
+cv::Mat TSlib::Tools::TensorToMat(const Tensor<double, Mode::GPU>& source)
 {
 	if (source.Dims() != 3)
 	{
