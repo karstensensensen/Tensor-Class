fb88d4a7a2ed8489e172ba6d9b679d7bdcae692d
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-03-06 21:36:57 +0100

    changed underlying datatype to unsigned char and added getter and setter declarations

--- Tensor/include/TensorBool.h
+++ Tensor/include/TensorBool.h
@@ -4,12 +4,12 @@
 
 namespace TSlib
 {
+	// in addition to adding some extra functions, this specialization also stores the bool values as one bit per bool instead of one byte per bool
 	template<Device device>
-	class Tensor<bool, device>: public Tensor<char, device>
+	class Tensor<bool, device>: public Tensor<unsigned char, device>
 	{
-
-		size_t bool_dim;
-		using Tensor<char, device>::m_shape;
+	protected:
+		std::vector<size_t> m_bool_shape;
 
 	public:
 		Tensor();
@@ -24,6 +24,23 @@ namespace TSlib
 
 		Tensor<bool, device>& Resize(const std::vector<size_t>& sizes, bool pad_val = false);
 
+		bool Get(const std::vector<size_t>& coords) const;
+		template<typename ... Args>
+		bool Get(const Args& ... coords) const;
+
+		bool At(size_t indx) const;
+
+		void SetIdx(bool val, size_t indx);
+		void Set(bool val, const std::vector<size_t>& coords);
+		template<typename ... Args>
+		void Set(bool val, const Args& ... coords);
+
+		void ToggleIdx(size_t indx);
+		void Toggle(const std::vector<size_t>& coords);
+		template<typename ... Args>
+		void Toggle(const Args& ... coords);
+
+		bool* Data() = delete;
 
 
 	};
